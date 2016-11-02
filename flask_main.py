303d6af8c1519978214658ed6a18097676e685db
mahananaka <jpaeschk@uoregon.edu>
2016-11-01 21:32:04 -0700

    fixed sorting

--- flask_main.py
+++ flask_main.py
@@ -125,7 +125,7 @@ def get_memos():
     can be inserted directly in the 'session' object.
     """
     records = [ ]
-    for record in collection.find( { "type": "dated_memo" } ).sort({ date:1}):
+    for record in collection.find( { "type": "dated_memo" } ).sort({ "date":1}):
         record['date'] = arrow.get(record['date']).isoformat()
         del record['_id']
         records.append(record)
