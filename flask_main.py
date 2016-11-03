4bef4b767abd8ab80a72393b6e1ece8e0aeae26e
mahananaka <jpaeschk@uoregon.edu>
2016-11-02 21:09:18 -0700

    changed routes

--- flask_main.py
+++ flask_main.py
@@ -144,7 +144,7 @@ def insert_memo(date, memo):
     document that is basically a dict.
     """
     record = {"type": "dated_memo", 
-              "date": arrow.get(date).to('local').isoformat(),
+              "date": arrow.get(date,"MM/DD/YYYY").to('local').isoformat(),
               "text": memo}
 
     collection.insert(record)
