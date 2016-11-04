6dee1eed68371cd384065c63e02ab98ffdbed7e2
mahananaka <jpaeschk@uoregon.edu>
2016-11-03 23:01:06 -0700

    adding tests

--- test_flask_main.py
+++ test_flask_main.py
@@ -24,18 +24,53 @@ from flask_main import get_memos
 from flask_main import insert_memo
 from flask_main import delete_memo
 
-#dbclient = MongoClient(MONGO_CLIENT_URL)            #connect
-#db = getattr(dbclient, secrets.client_secrets.db)   #get specificed db in secrets
-#collection = db.dated                               #get dated collection/table
+dbclient = MongoClient(MONGO_CLIENT_URL)            #connect
+db = getattr(dbclient, secrets.client_secrets.db)   #get specificed db in secrets
+collection = db.dated                               #get dated collection/table
 
 def test_humanize_arrow_date():
     """
-    Testing initializing Humanize
+    Testing Humanize
     """
-
     curDateTime = arrow.utcnow().to('local')
 
     assert humanize_arrow_date(curDateTime) == "Today"
     assert humanize_arrow_date(curDateTime.replace(days=+1)) == "Tomorrow"
     assert humanize_arrow_date(curDateTime.replace(days=-1)) == "Yesterday"
+
+def test_insert_memo():
+    """
+    Testing insertion into db
+    """
+    assert insert_memo("02/30/2016","Testing a failed insert") == False
+    assert insert_memo("11/12/2016","This entry is created by nose") == True
+
+def test_get_memos():
+    """
+    Testing memos retrieval
+    """
+    records = get_memos()
+
+    assert len(records) > 0 #We inserted in last test so this should not be 0
+
+    #veryify the proper form of the dict
+    for entry in records:
+        assert entry['date'] is not None
+        assert entry['text'] is not None
+
+def test_get_memos():
+    """
+    Testing memo delete
+    """
+    key = {'text':'This entry is created by nose'} #This entry exists if insertion testing passed.
+    foundAndDelted = False
+
+    for record in collection.find(key):
+        collection.remove({'_id':record['_id']})
+        foundAndDelted = True
+
+    assert foundAndDelted == True
+
+
+
 
