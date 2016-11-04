45e437eacc800342221ce97227b225326f796cf8
mahananaka <jpaeschk@uoregon.edu>
2016-11-03 22:03:23 -0700

    Adding Tests

---
+++ test_flask_main.py
@@ -0,0 +1,41 @@
+"""
+Nose tests for flask_main.py
+"""
+
+# Mongo database
+#from bson.objectid import ObjectId
+#from pymongo import MongoClient
+#import secrets.admin_secrets
+#import secrets.client_secrets
+#MONGO_CLIENT_URL = "mongodb://{}:{}@localhost:{}/{}".format(
+#    secrets.client_secrets.db_user,
+#    secrets.client_secrets.db_user_pw,
+#    secrets.admin_secrets.port, 
+#    secrets.client_secrets.db)
+
+# Date handling 
+import arrow # Replacement for datetime, based on moment.js
+import datetime # But we still need time
+from dateutil import tz  # For interpreting local times
+
+# module we are testing
+from flask_main import humanize_arrow_date
+from flask_main import get_memos
+from flask_main import insert_memo
+from flask_main import delete_memo
+
+#dbclient = MongoClient(MONGO_CLIENT_URL)            #connect
+#db = getattr(dbclient, secrets.client_secrets.db)   #get specificed db in secrets
+#collection = db.dated                               #get dated collection/table
+
+def test_humanize_arrow_date():
+    """
+    Testing initializing AcpBrevet
+    """
+
+    curDateTime = arrow.utcnow().to('local')
+
+    assert flask_main.humanize_arrow_date(curDateTime) == "Today"
+    assert flask_main.humanize_arrow_date(curDateTime.replace(days=+1)) == "Tomorrow"
+    assert flask_main.humanize_arrow_date(curDateTime.replace(days=-1)) == "Yesterday"
+
